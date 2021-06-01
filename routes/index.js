851c8481c7f68e7d7a308faeb65f2c38112e1d9d
crist <cristhian-zambrano@upc.edu.co>
2021-06-01 17:39:53 -0500

    vista login realizada

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('login');
 });
-
+router.get('/inicio', function(req, res, next) {
+  res.render('frontadidas');
+});
 module.exports = router;
